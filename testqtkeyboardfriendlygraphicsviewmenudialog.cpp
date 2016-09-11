6dc4573ef6bbacd16f0300fa6e36fbc9e2391da1
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-11 14:01:50 +0200

    Go Travis

--- testqtkeyboardfriendlygraphicsviewmenudialog.cpp
+++ testqtkeyboardfriendlygraphicsviewmenudialog.cpp
@@ -72,18 +72,9 @@ ribi::Help ribi::TestKeyboardFriendlyGraphicsViewMenuDialog::GetHelp() const noe
   );
 }
 
-boost::shared_ptr<const ribi::Program> ribi::TestKeyboardFriendlyGraphicsViewMenuDialog::GetProgram() const noexcept
-{
-  const boost::shared_ptr<const Program> p {
-    new ProgramTestQtKeyboardFriendlyGraphicsView
-  };
-  assert(p);
-  return p;
-}
-
 std::string ribi::TestKeyboardFriendlyGraphicsViewMenuDialog::GetVersion() const noexcept
 {
-  return "1.0";
+  return "1.1";
 }
 
 std::vector<std::string> ribi::TestKeyboardFriendlyGraphicsViewMenuDialog::GetVersionHistory() const noexcept
@@ -93,18 +84,7 @@ std::vector<std::string> ribi::TestKeyboardFriendlyGraphicsViewMenuDialog::GetVe
     "2012-12-17: version 0.2: initial release, added about sceen, adding items, showing help",
     "2012-12-19: version 0.3: improved looks, added QtArrowItem, QtPathArrowItem, QtRoundedTextRect",
     "2013-11-05: version 0.4: conformized for ProjectRichelBilderbeekConsole",
-    "2015-10-02: version 1.0: moved to own GitHub"
+    "2015-10-02: version 1.0: moved to own GitHub TestKeyboardFriendlyGraphicsView",
+    "2016-09-11: version 1.1: moved to own GitHub QtKeyboardFriendlyGraphicsView"
   };
 }
-
-#ifndef NDEBUG
-void ribi::TestKeyboardFriendlyGraphicsViewMenuDialog::Test() noexcept
-{
-  {
-    static bool is_tested{false};
-    if (is_tested) return;
-    is_tested = true;
-  }
-  const TestTimer test_timer(__func__,__FILE__,1.0);
-}
-#endif
