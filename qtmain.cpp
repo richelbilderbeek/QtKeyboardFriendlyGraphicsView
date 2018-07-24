c08fd23df9bf44403931c405352646af8acedf39
richelbilderbeek <richel@richelbilderbeek.nl>
2018-07-24 15:04:13 +0200

    EOD

--- qtmain.cpp
+++ qtmain.cpp
@@ -1,16 +1,10 @@
-
-
-
-
 #include <QApplication>
 #include <QDesktopWidget>
 #include "qtkeyboardfriendlygraphicsviewdemomenudialog.h"
 
-
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-
   ribi::QtTestKeyboardFriendlyGraphicsViewMenuDialog d;
   d.show();
   return a.exec();
